bd86d96eccfe8b7f487f56cebcd1a660d4dce2c4
kpaul <kpaulina1995@gmail.com>
2021-11-07 17:31:06 +0100

    ćw1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+       int i = 1000;
+
+        do {
+            if(i % 87 == 0)
+            System.out.println("Liczba " + i + " jest podzielna przez 87" );
+        }
+            while (--i > 0);
+
 
     }
 }
