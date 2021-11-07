94acfe683a487ebab48e756546241f151aa79f0e
kpaul <kpaulina1995@gmail.com>
2021-11-07 19:44:59 +0100

    ćw2

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,37 @@ public class Main {
           a) ta liczba jest parzysta --> podpowiedz switch(a % 2) {
           b) program który określi czy liczba jest podzielna przez 7
          */
+        Scanner scan = new Scanner((System.in));
+        System.out.print("Podaj liczbę ");
+        int a = scan.nextInt();
+        switch (a % 2)
+        {
+            case 0:
+                System.out.println("Liczba jest parzysta");
+                break;
+            default:
+                System.out.println("Liczba jest nieparzysta");
+                break;
 
+        }
+        System.out.println("Podaj liczbę");
+        int b = scan.nextInt();
+        switch (b % 7)
+        {
+            case 0:
+                System.out.println("Liczba jest podzielna przez 7");
+                break;
+            default:
+                System.out.println("Liczba jest niepodzielna przez 7");
+                break;
+
+        }
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
+
 
 
     }
